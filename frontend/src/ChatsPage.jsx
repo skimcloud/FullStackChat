bb487d0b0d1793ea8dc89f5932751ba220308681
skimcloud <113874029+skimcloud@users.noreply.github.com>
2024-01-11 05:42:58 -0800

    Init

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,17 @@
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        'c81069b4-d4e8-4e35-b4b9-fb8038a28748',
+        props.user.username,
+        props.user.secret
+    );
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: '100%' }} />
+        </div>
+    )
+}
+
+export default ChatsPage
